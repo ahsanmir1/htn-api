da4da31a7fef3d228e0d644f64e6f27ff7413ed4
ahsanayoub <113545458+ahsanayoub@users.noreply.github.com>
2026-09-08 11:11:34 -0500

    feat: register discovery API routes

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import jobsRouter from "./routes/jobs.js";
 import applicationsRouter from "./routes/applications.js";
 import resumesRouter from "./routes/resumes.js";
 import candidatesRouter from "./routes/candidates.js";
+import discoveryRouter from "./routes/discovery.js";
 
 const app = express();
 
@@ -20,5 +21,6 @@ app.use("/api/jobs", jobsRouter);
 app.use("/api/applications", applicationsRouter);
 app.use("/api/resumes", resumesRouter);
 app.use("/api/candidates", candidatesRouter);
+app.use("/api/discovery", discoveryRouter);
 
 export default app;
